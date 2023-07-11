d2d7137b9c287311c2c16c9f03f275a45ae4d6d6
Rubén Gázquez Rosales <rgazquez@seyte.com>
2023-07-11 13:26:35 +0200

    removed node_modules from git and updated eslint config

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint/eslint-plugin",
-    "@seyte/eslint-plugin-nest-decorator-scope-request",
-  ],
+  plugins: ["@typescript-eslint/eslint-plugin", "nest-decorator-scope-request"],
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
@@ -24,6 +21,6 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@seyte/nest-decorator-scope-request": "error",
+    "nest-decorator-scope-request": "error",
   },
 };
