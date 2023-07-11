893783edfb6df8228ed609e1f85f480bcb3d7c17
Rubén Gázquez Rosales <rgazquez@seyte.com>
2023-07-11 14:11:17 +0200

    updated eslint configuration & update packague version to 1.1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
+/* eslint-env node */
 module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint/eslint-plugin", "@seyte/eslint-plugin-nest-decorator-scope-request"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  plugins: ["@typescript-eslint", "@seyte"],
+  root: true,
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "nest-decorator-scope-request": "error",
+    "@seyte/nest-decorator-scope-request": "error",
   },
 };
