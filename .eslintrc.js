b4fc644f0c7c91038e08e602f4dabd9233735fa8
Rubén Gázquez Rosales <rgazquez@seyte.com>
2023-07-12 12:02:24 +0200

    updated to v1.2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "@seyte"],
+  plugins: ["@typescript-eslint", "nest_decorator_scope_request"],
   root: true,
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@seyte/nest-decorator-scope-request": "error",
+    "no-case-declarations": "off",
+    "@typescript-eslint/adjacent-overload-signatures": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "nest_decorator_scope_request/rule": "error",
   },
 };
